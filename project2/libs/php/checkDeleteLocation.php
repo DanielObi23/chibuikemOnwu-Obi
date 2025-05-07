8773307e290b7cc54bffc7f9d080a41498280189
Chibuikem Daniel Onwu-Obi <chibuikemonwuobi23@gmail.com>
2025-05-07 13:51:12 +0100

    Add files via upload

---
+++ project2/libs/php/checkDeleteLocation.php
@@ -0,0 +1,74 @@
+<?php
+
+	// example use from browser
+	// http://localhost/companydirectory/libs/php/deleteLocationByID.php?id=<id>
+
+	// remove next two lines for production
+	ini_set('display_errors', 'On');
+	error_reporting(E_ALL);
+
+	$executionStartTime = microtime(true);
+
+	include("config.php");
+
+	header('Content-Type: application/json; charset=UTF-8');
+
+	$conn = new mysqli($cd_host, $cd_user, $cd_password, $cd_dbname, $cd_port, $cd_socket);
+
+	if (mysqli_connect_errno()) {
+		
+		$output['status']['code'] = "300";
+		$output['status']['name'] = "failure";
+		$output['status']['description'] = "database unavailable";
+		$output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
+		$output['data'] = [];
+
+		mysqli_close($conn);
+
+		echo json_encode($output);
+
+		exit;
+	}	
+
+	$checkQuery = $conn->prepare('SELECT COUNT(id) as departmentCount FROM department WHERE locationID = ?');
+	$checkQuery->bind_param("i", $_GET['id']);
+	$checkQuery->execute();
+	
+	if (false === $checkQuery) {
+		$output['status']['code'] = "400";
+		$output['status']['name'] = "executed";
+		$output['status']['description'] = "check query failed";	
+		$output['data'] = [];
+
+		mysqli_close($conn);
+
+		echo json_encode($output); 
+		exit;
+	}
+	
+	$result = $checkQuery->get_result();
+	$row = $result->fetch_assoc();
+	
+	// If the location is used in departments, return an error
+	if ($row['departmentCount'] > 0) {
+		$output['status']['code'] = "409"; 
+		$output['status']['name'] = "conflict";
+		$output['status']['description'] = "Cannot delete location because it is being used by " . $row['departmentCount'] . " department(s)";
+		$output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
+		$output['data'] = [];
+		
+		mysqli_close($conn);
+		echo json_encode($output);
+		exit;
+	} else {
+		$output['status']['code'] = "200";
+		$output['status']['name'] = "ok";
+		$output['status']['description'] = "Location can be deleted";
+		$output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
+		$output['data'] = [];
+	}
+    
+	
+	mysqli_close($conn);
+	echo json_encode($output); 
+?>
---
+++ project2/libs/php/config.php
@@ -0,0 +1,19 @@
+
+<?php
+
+	// connection details for MySQL database
+
+	$cd_host = "127.0.0.1";
+	$cd_port = 3306;
+	$cd_socket = "";
+
+	// database name, username and password
+
+	$cd_dbname = "companydirectory";
+	$cd_user = "dani";
+	$cd_password = "K1ng6l@z3";
+
+	// $cd_dbname = "u758236859_directory";
+	// $cd_user = "u758236859_dani";
+	// $cd_password = "K1ng6l@z3";
+?>
